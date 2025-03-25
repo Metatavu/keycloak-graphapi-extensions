081c36c27749eaa957891faf8a90acf49f80e1cc
Daniilmass <daniil.kings@gmail.com>
2025-03-25 14:48:54 +0200

    Support for leave user groups as paths

--- src/main/java/fi/metatavu/keycloak/graphapi/GraphApiGroupsIdentityProviderMapper.java
+++ src/main/java/fi/metatavu/keycloak/graphapi/GraphApiGroupsIdentityProviderMapper.java
@@ -81,6 +81,20 @@ public class GraphApiGroupsIdentityProviderMapper extends AbstractIdentityProvid
     }
 
     /**
+     * Returns true if group name matches managed Keycloak group names / paths
+     *
+     * @param groupName group name
+     * @param managedKeycloakGroupNames group names / paths
+     */
+    private boolean isGroupNameMatch(String groupName, List<String> managedKeycloakGroupNames) {
+        return managedKeycloakGroupNames.stream().anyMatch(name -> {
+            String[] parts = name.split("/");
+            String lastPart = parts[parts.length - 1];
+            return lastPart.equals(groupName) || name.equals(groupName);
+        });
+    }
+
+    /**
      * Updates user manager attributes
      *
      * @param user user model
@@ -109,7 +123,7 @@ public class GraphApiGroupsIdentityProviderMapper extends AbstractIdentityProvid
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         ArrayList<GroupModel> leaveUserGroups = new ArrayList<>(user.getGroupsStream()
-            .filter(group -> managedKeycloakGroupNames.contains(group.getName()))
+            .filter(group -> isGroupNameMatch(group.getName(), managedKeycloakGroupNames))
             .toList());
 
         List<String> previousGroupNames = leaveUserGroups.stream()
